fdec3d34a45976b8bec954836a84509040c05b8b
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2021-08-13 18:59:51 -0300

    Tarefa 2 - Passo 2

--- cypress/integration/meu_teste_end_to_end.js
+++ cypress/integration/meu_teste_end_to_end.js
@@ -2,6 +2,9 @@ describe('Teste End-to-End', () => {
     it('Meu Primeiro Teste', () => {
         // abre o site
         cy.visit('http://localhost:5000/')
+        
+        // Verifica se existe o livro desejado
+        cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
     })
   })
 
